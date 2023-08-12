e5173e238e301c7b5c8a96764ea0ab0232b834d0
omar3adwy <3adawy42@gmail.com>
2023-08-11 19:04:10 -0700

    a

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - entery point
+ *
+ * Description: print alphabet letters except q and e
+ *
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		if (ch == 'e' || ch == 'q')
+			 ch++;
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+
+	return (0);
+}
