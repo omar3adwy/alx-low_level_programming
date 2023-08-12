7f3f44c238ed8c3037c459d68056d55bdbfcdb31
omar3adwy <3adawy42@gmail.com>
2023-08-12 06:58:23 -0700

    a

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,14 +11,14 @@
 
 int main(void)
 {
-	int digit = 0, digit2;
+	int digit1 = 0, digit2;
 
 	while (digit1 <= 9)
 	{
 		digit2 = 0;
 		while (digit2 <= 9)
 		{
-			if (digit != digit2 && digit1 < digit2)
+			if (digit1 != digit2 && digit1 < digit2)
 			{
 				putchar(digit1 + 48);
 				putchar(digit2 + 48);
