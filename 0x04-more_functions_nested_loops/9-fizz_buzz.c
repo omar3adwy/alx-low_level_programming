5b0b40e71aca33498d36597bbdf2c23b9bf08dea
omar3adwy <3adawy42@gmail.com>
2023-08-18 14:08:38 -0700

    a

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -18,9 +18,9 @@ int main(void)
 	{
 		if (num % 3 == 0 && !(num % 5 == 0))
 			printf("Fizz");
-		else if (num % 5 == 0 && !(num % 3 == 0))
+		else if (num % 5 == 0 && !(num % 3 == 0)
 			printf("Buzz");
-		else if (num % 3 == 0 && !(num % 5 == 0))
+		else if (num % 3 == 0 && !(num % 5 == 0)
 			printf("FizzBuzz");
 		else
 			printf("%d", num);
