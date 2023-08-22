5c610cec9434d2494a5b858096c4b2bee5f8ab5e
omar3adwy <3adawy42@gmail.com>
2023-08-21 17:55:17 -0700

    a

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -16,9 +16,9 @@ void print_array(int *a, int n)
 	for (i = 0; i < n; i++)
 	{
 		if (i != (n - 1))
-			print("%d, ", a[i]);
+			printf("%d, ", a[i]);
 		else
-			print("%d", a[i]);
+			printf("%d", a[i]);
 	}
 	printf("\n");
 }
