4695efe5ce6d968135f137b14cfa327f2f5e3b03
omar3adwy <3adawy42@gmail.com>
2023-08-28 11:13:21 -0700

    a

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_chessboard - points theseaoard
+ *
+ * @a: revs
+ * Return: nothing.
+*/
+
+void print_chessboard(char (*a)[8])
+{
+	int itr, jtr;
+
+	for (itr = 0; itr < 8; jtr++)
+	{
+		for (jtr = 0; jtr < 8; jtr++)
+		{
+			_putchar(a[itr][jtr]);
+		}
+		_putchar('\n');
+	}
+}
