0c85591491dafc0b9d77f4ffaf7381300314eab0
omar3adwy <3adawy42@gmail.com>
2023-09-06 03:41:29 -0700

    a

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -33,7 +33,7 @@ char *argstostr(int ac, char **av)
 	for (; i < ac; i++, nc++)
 		nc += _strlen(av[i]);
 
-	s = malloc(sizeof(char) * nc + 1)
+	s = malloc(sizeof(char) * nc + 1);
 	if (s == 0)
 		return (NULL);
 
