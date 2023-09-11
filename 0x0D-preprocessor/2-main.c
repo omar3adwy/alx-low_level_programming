a79db94eebd61e5972d9a4267a980bd96cef8b8a
omar3adwy <3adawy42@gmail.com>
2023-09-11 01:57:01 -0700

    a

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main_ print name of of this source file
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,6 @@
+#ifndef _FUNCTION_LIKE_MACRO_H
+#define _FUNCTION_LIKE_MACRO_H
+
+#define ABC(x) ((X) < 0 ? (-(x)) : (X))
+
+#endif /* _FUNCTION_LIKE_MACRO_H */
