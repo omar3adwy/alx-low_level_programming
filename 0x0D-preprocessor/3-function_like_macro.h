c8af1782480a185d0975bf007facb85119919357
omar3adwy <3adawy42@gmail.com>
2023-09-11 02:17:21 -0700

    a

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,6 @@
 #ifndef _FUNCTION_LIKE_MACRO_H
 #define _FUNCTION_LIKE_MACRO_H
 
-#define ABC(x) ((X) < 0 ? (-(x)) : (X))
+#define ABS(x) ((X) < 0 ? (-(x)) : (X))
 
 #endif /* _FUNCTION_LIKE_MACRO_H */
