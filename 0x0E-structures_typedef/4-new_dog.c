128bc9764154c4f71dea3aa0c6ad6144abc5f361
omar3adwy <3adawy42@gmail.com>
2023-09-11 17:17:51 -0700

    a

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -50,6 +50,9 @@ char *_strcopy(char *dest, char *src)
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
+	if (d == NULL)
+		return (NULL);
+
 	dog_t *dog;
 
 	/* if name and owner are empty and age is less than zero return null*/
