a245449ba8ab78fc5d06fa811322e6ff0d551829
omar3adwy <3adawy42@gmail.com>
2023-09-14 03:17:00 -0700

    a

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -18,7 +18,7 @@ void format_char(char *separator, va_list ap)
  */
 
 void format_int(char *separator, va_list ap)
-{       
+{
 	printf("%s%d", separator, va_arg(ap, int));
 }
 
@@ -39,4 +39,38 @@ void format_string(char *separator, va_list ap)
 	printf("%s%s", separator, va_arg(ap, int));
 }
 
+/**
+ * print_all - prints anything
+ * @format: the format string
+ */
 
+void print_all(const char * const format, ...)
+{
+	int i = 0, j;
+	char *separator = "";
+	va_list ap;
+	token_t tokens[] = {
+		{"c", format_char},
+		{"i", format_int},
+		{"f", format_float},
+		{"s", format_string},
+	};
+
+	va_start(ap, format);
+	while (format && format[i])
+	{
+		j = 0;
+		while (tokens[j], token)
+		{
+			if (format[i] == tokens[j], token[0])
+			{
+				tokens[j].f(separator, ap);
+				separator = ", ";
+			}
+			j++;
+		}
+		i++;
+	}
+	printf("\n");
+	va_end(ap);
+}
