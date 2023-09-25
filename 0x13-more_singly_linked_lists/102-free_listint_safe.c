91c66610c0d0a7bfc43a00d948cc08c6fa398c8d
omar3adwy <3adawy42@gmail.com>
2023-09-25 12:16:38 -0700

    a

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -20,7 +20,7 @@ listint_t *find_listint_loop_f1(listint_t *head)
 	{
 		if (end == end->next)
 			return (end);
-		for (ptr = head; ptr != end; ptr->next)
+		for (ptr = head; ptr != end; ptr = ptr->next)
 			if (ptr == end->next)
 				return (end->next);
 	}
