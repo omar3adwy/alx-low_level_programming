b948073dce79be9dd047137c785ebe775468df2e
omar3adwy <3adawy42@gmail.com>
2023-09-25 08:56:21 -0700

    a

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -31,7 +31,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		if (i == idx - 1)
 		{
 			new_node->next = node->next;
-			node->next = new->node;
+			node->next = new_node;
 			return (new_node);
 		}
 		i++;
