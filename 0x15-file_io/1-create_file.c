4099bfc5142f9e5a69c64cb4c19de0300c8306e2
omar3adwy <3adawy42@gmail.com>
2023-10-02 15:56:02 -0700

    a

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -38,7 +38,7 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 	if (len)
-		bytes = writes(fd, text_content, len);
+		bytes = write(fd, text_content, len);
 	close(fd);
 	return (bytes == len ? 1 : -1);
 }
