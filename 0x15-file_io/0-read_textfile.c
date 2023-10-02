61f6fa45ec0a43e571ef2b714bdcf78e0eb61fbc
omar3adwy <3adawy42@gmail.com>
2023-10-02 12:56:11 -0700

    a

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -21,6 +21,6 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	bytes = read(fd, &buf[0], letters);
 	bytes = write(STDOUT_FILENO, &buf[0], bytes);
-	close(fd)
+	close(fd);
 	return (bytes);
 }
