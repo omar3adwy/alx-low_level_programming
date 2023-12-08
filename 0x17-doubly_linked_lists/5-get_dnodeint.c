c15232169a5fdfc9e576f6847d3394c0f769465f
omar3adwy <3adawy42@gmail.com>
2023-12-07 18:48:26 -0800

    dll

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - returns node at giver index
+ * @head: pointer of node to return
+ * #index:address og node at index
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int i = 0;
+
+	while (head)
+	{
+		if (i ==  index)
+			return (head);
+		head = head->next;
+		i++;
+	}
+	return (NULL);
+
