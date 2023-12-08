063de24178980125180aa12e2f180596d5a3fc57
omar3adwy <3adawy42@gmail.com>
2023-12-07 18:56:17 -0800

    dll

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -14,7 +14,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	if (!head || !new)
 		return (new ? free(new), NULL : NULL);
 
-	new->n = n ;
+	new->n = n;
 	new->prev = NULL;
 	if (!*head)
 	{
