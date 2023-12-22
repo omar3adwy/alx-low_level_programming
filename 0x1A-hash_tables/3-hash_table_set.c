a3f30f7cc36fcbab49f270eecc1f377982b95a99
omar3adwy <3adawy42@gmail.com>
2023-12-21 16:54:42 -0800

    a

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -46,7 +46,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
         char *new_value;
 
         if (ht == NULL || ht->array == NULL || ht->size == 0 ||
-                key == NULL || strlen(key) == 0 || value == NULL
+                key == NULL || strlen(key) == 0 || value == NULL)
                 return (0);
         index = key_index((const unsigned char *)key, ht->size);
         tmp = ht->array[index];
